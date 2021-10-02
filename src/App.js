df73f1509fae3124419e901c99cf43e55d7a506d
Will Smart <wjsmart12@gmail.com>
2021-10-02 14:34:30 -0500

    Basic print

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+const userHand = { 
+  "card1": { "value" : 'A', "suit" : 'H'}, 
+  "card2": { "value" : 'A', "suit" : 'D'}
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Card 1: {userHand.card1.value}{userHand.card1.suit}</h1>
+      <h1>Card 2: {userHand.card2.value}{userHand.card2.suit}</h1>
+      <h1>Action: Raise!</h1>
     </div>
   );
 }
